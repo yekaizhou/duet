dace8d433721338a5c731e2edbe5c08af07115f2
Yekai Zhou <ykzhou@bal15.cs.hku.hk>
2021-07-27 11:07:20 +0800

    add license and update setup

--- setup.py
+++ setup.py
@@ -6,20 +6,18 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(
-    name = "duet",
-    # version = "0.1",
-    description = "Fast and Accurate Novel SV Calling and Haplotyping",
-    author = "Yekai Zhou",
-    author_email = "yekai.zhou@outlook.com",
-    url = "https://github.com/yekaizhou/duet",
-    # license = "MIT",
-    packages = find_packages(include="*.py"),
-    package_dir = {"": "src"},
-    # data_files = [("", ["LICENSE"])],
+    name = 'duet',
+    version = '0.1',
+    description = 'Fast and Accurate Novel SV Calling and Haplotyping',
+    author = 'Yekai Zhou',
+    author_email = 'yekai.zhou@outlook.com',
+    url = 'https://github.com/yekaizhou/duet',
+    license = 'BSD',
+    packages = find_packages(where = 'src'),
+    package_dir = {'': 'src'},
+    data_files = [('', ['LICENSE'])],
     scripts=['src/duet/duet'],
-    # long_description = LONG_DESCRIPTION,
     long_description = readme,
     long_description_content_type = 'text/markdown',
     zip_safe = False,
-    # install_requires = ['pysam', 'Biopython', 'Cigar', 'numpy', 'pyvcf']
 )
