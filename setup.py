2a8802c554031c0651c4c2be53d243179b1f46e6
Yekai Zhou <yekai.zhou@outlook.com>
2021-07-28 17:02:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(
     name = 'duet',
     version = '0.2',
-    description = 'SNP-Assisted SV Calling and Phasing from Low-depth ONT',
+    description = 'SNP-Assisted Phased SV Detection from Low-depth ONT',
     author = 'Yekai Zhou',
     author_email = 'yekai.zhou@outlook.com',
     url = 'https://github.com/yekaizhou/duet',
